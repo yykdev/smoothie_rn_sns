d7481d337a6a78483bebb9c51ebf960c1fc08c0e
yykdev <assavictory@gmail.com>
2019-12-28 01:47:21 +0900

    바벨, 타입스크립트 등 프로젝트 기본 설정 셋팅

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,9 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const App: () => React$Node = () => {
+interface Props {}
+
+const App = ({}: Props) => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
